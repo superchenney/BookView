35e6d20658a7f80b52a73a92e535441e96a0890a
superchenney <superchenney@gmail.com>
2017-04-20 21:46:04 +0800

    develop init

---
+++ bookview_fornt/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+
+    <Topbar></Topbar>
+    <router-view></router-view>
+    
+  </div>
+</template>
+
+<script>
+import Topbar from '@/components/Topbar'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+    };
+  },
+  methods: {
+   
+  },
+  components:{ Topbar }
+}
+</script>
+
+<style>
+body{
+  padding: 0;
+  margin: 0;
+/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;*/
+}
+
+#app {
+/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+}
+</style>
---
+++ bookview_fornt/src/components/Topbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="topbar">
+		<el-menu theme="light" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" >
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+  data() {
+    return {
+      activeIndex: '1',
+    }
+  },
+  mounted() {
+    // el 被新创建的 vm.$el 替换，并挂载到实例上去之后调用该钩子
+
+    // this.loadCityList();
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .topbar{
+    position: relative;
+    /*width: 1088px;*/
+    margin: 0 auto;
+    background-color: #eef1f6;
+  }
+</style>
